2712baa61bd7b5e490b2c0e8d228db0bd786eba4
jeremywade <jeremy.wade.10@gmail.com>
2025-04-29 10:10:17 -0500

    Fixed challenge syntax bug

---
+++ src/challenges.js
@@ -0,0 +1,11 @@
+const challenges = [
+    {
+      id: 1,
+      question: "What’s a benefit of AI in education?",
+      options: ["Faster grading", "Increased screen time", "Higher tuition"],
+      correct: 0
+    },
+    ...
+  ];
+  
+  export default challenges;
